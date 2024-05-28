49bf1394f0780c44b7e682a0ab948ddcd421a77b
Faiz Khan <faizkhan@Faizs-MacBook-Air.local>
2024-05-27 23:12:59 -0500

    reqs test

--- openaicli.py
+++ openaicli.py
@@ -1,1 +1,13 @@
+import code
+import openai
 
+def promptAI():
+
+def runlumina(prompt=''):
+  while True:
+    print(" -> ", end='')
+    aa=input()
+    if aa == 'exit':
+      raise EOFError()
+  
+code.interact(banner="LUMINA ONLINE...",exitmsg="LUMINA OFFLINE...", readfunc=runlumina)
